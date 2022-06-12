272de1bd8af7c4e1e60cc3acc896fffafe5483f9
root <root@DESKTOP-3REDR5J.localdomain>
2022-06-12 19:55:58 -0400

    Finished configuration Stripe

---
+++ src/services/stripe.ts
@@ -0,0 +1,12 @@
+import Stripe from 'stripe';
+
+import { version } from '../../package.json';
+
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+export const stripe = new Stripe(process.env.STRIPE_API_KEY!, {
+  apiVersion: `2020-08-27`,
+  appInfo: {
+    name: `Ignews`,
+    version,
+  },
+});

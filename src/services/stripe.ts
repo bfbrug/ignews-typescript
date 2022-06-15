beaabe1f237a227bd8b9d016a9763c1c160394d9
root <root@DESKTOP-3REDR5J.localdomain>
2022-06-14 22:57:17 -0400

    Configurado Fauna DB

--- src/services/stripe.ts
+++ src/services/stripe.ts
@@ -4,9 +4,9 @@ import { version } from '../../package.json';
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 export const stripe = new Stripe(process.env.STRIPE_API_KEY!, {
-  apiVersion: `2020-08-27`,
+  apiVersion: '2020-08-27',
   appInfo: {
-    name: `Ignews`,
+    name: 'Ignews',
     version,
   },
 });
